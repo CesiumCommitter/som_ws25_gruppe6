92b06eef667fe7cf5fd839e883361c23e40308e3
SCHILLER Samuel <SSCHF5F8@eu.airbus.corp>
2025-12-09 18:15:04 +0100

    updated sendMQTTShort

--- p2_raspi/sendMQTTShort.py
+++ p2_raspi/sendMQTTShort.py
@@ -4,10 +4,13 @@ from random import randint
 MQTT_PORT=1883
 MQTT_ADDRESS="127.0.0.1"
 MQTT_CLIENT_NAME="Test_PiPub"
-MQTT_TOPIC="Gruppenname/Kanal"
+MQTT_TOPIC="dht11/temp"
 
-#Hier steht die Nachricht
-text="Random Value: " + str(randint(1, 100))
+timestamp = None
+temp_value = str(randint(1, 100))
+
+#Hier entsteht die Nachricht
+text = f"{temp_value}_{timestamp}"
 
 #Send Text to MQTT
 client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1,MQTT_CLIENT_NAME)
