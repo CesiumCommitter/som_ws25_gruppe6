f436d9e904679984833b24a1448feee17ae0151c
SCHILLER Samuel <SSCHF5F8@eu.airbus.corp>
2025-12-11 15:40:11 +0100

    Update DHT_Sensor.py

--- p2_raspi/DHT_Sensor.py
+++ p2_raspi/DHT_Sensor.py
@@ -3,19 +3,15 @@ import board
 import adafruit_dht
 
 class DHTSensor:
-    def __init__(self, pin=board.D23):
-        # Initialisieren Sie das dht-Gerät, wobei der Datenpin mit Pin 16 (GPIO 23) des Raspberry Pi verbunden ist:
-        # dhtDevice = adafruit_dht.DHT11(board.D23)
-        self.device = adafruit_dht.DHT11(pin)
+    def __init__(self):
+        self.device = adafruit_dht.DHT11(board.D23)
 
     def dht_read_temp(self):
         while True:
             try:
                 temperature_c = self.device.temperature
-                #print(f"Temp: {round(temperature_c)} C")
                 return temperature_c
             except RuntimeError as error:
-                # Fehler passieren ziemlich oft, DHT's sind schwer zu lesen, einfach weitermachen
                 print(error.args[0])
                 time.sleep(2.0)
                 continue
