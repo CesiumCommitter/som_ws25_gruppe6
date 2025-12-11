e85aee8ebeaebe5b8517f3def57388dd4c8ceb7c
SCHILLER Samuel <SSCHF5F8@eu.airbus.corp>
2025-12-11 15:23:27 +0100

    changed usepulseio in Sensor Class

--- p2_raspi/DHT_Sensor.py
+++ p2_raspi/DHT_Sensor.py
@@ -6,7 +6,7 @@ class DHTSensor:
     def __init__(self, pin=board.D23):
         # Initialisieren Sie das dht-Gerät, wobei der Datenpin mit Pin 16 (GPIO 23) des Raspberry Pi verbunden ist:
         # dhtDevice = adafruit_dht.DHT11(board.D23)
-        self.device = adafruit_dht.DHT11(pin)
+        self.device = adafruit_dht.DHT11(pin, use_pulseio=False)
 
     def dht_read_temp(self):
         while True:
