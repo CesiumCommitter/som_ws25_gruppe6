8cb3dac86447e4e01754ee1be6950a5e42f34299
CesiumCommitter <167583394+CesiumCommitter@users.noreply.github.com>
2025-12-16 16:51:49 +0100

    fixed read_MQTT.py
    Now accesses values correctly

--- p2_raspi/readMQTT.py
+++ p2_raspi/readMQTT.py
@@ -4,7 +4,7 @@ import time
 MQTT_PORT = 1883
 MQTT_ADDRESS = "141.22.36.200"
 MQTT_CLIENT_NAME = "Gruppe6_PiSub"
-MQTT_TOPIC = "Gruppenname/Kanal"
+MQTT_TOPIC = "dht11/temp"
 TICK_RATE_HZ = 2
 TICK_RATE = 1/TICK_RATE_HZ
 
@@ -12,11 +12,11 @@ TICK_RATE = 1/TICK_RATE_HZ
 message_queue = []
 
 def on_message(client, userdata, msg):
-    message=msg.payload.decode()
+    message = msg.payload.decode()
     message_queue.append(message)
 
-client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1,MQTT_CLIENT_NAME)
-client.connect(MQTT_ADDRESS,MQTT_PORT)
+client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, MQTT_CLIENT_NAME)
+client.connect(MQTT_ADDRESS, MQTT_PORT)
 client.subscribe(MQTT_TOPIC)
 client.on_message = on_message
 client.loop_start()
@@ -28,6 +28,7 @@ while (True):
     while len(message_queue)>0:
         last_value=message_queue.pop()
         value_queue.append(last_value)
-        #print(last_value)
-        
-        
+        vals = last_value.split("_", 3)
+        int_temp = int(vals[0])
+        int_humidity = int(vals[1])
+        str_timestamp = vals[2]
--- p2_raspi/sensordata_gui.py
+++ p2_raspi/sensordata_gui.py
@@ -1,26 +1,58 @@
-#import serial
+# For MQTT
+from paho.mqtt import client as mqtt_client
+import time
+
+
+# For GUI
 from tkinter import *
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import random
 
+
+# Define MQTT Source
+MQTT_PORT = 1883
+MQTT_ADDRESS = "141.22.36.200"
+MQTT_CLIENT_NAME = "Gruppe6_PiSub"
+MQTT_TOPIC = "dht11/temp"
+TICK_RATE_HZ = 2
+TICK_RATE = 1/TICK_RATE_HZ
+
+
+# Start MQTT Listening
+message_queue = []
+def on_message(client, userdata, msg):
+    message = msg.payload.decode()
+    message_queue.append(message)
+client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, MQTT_CLIENT_NAME)
+client.connect(MQTT_ADDRESS, MQTT_PORT)
+client.subscribe(MQTT_TOPIC)
+client.on_message = on_message
+client.loop_start()
+
+
+# Define GUI
 root = Tk()
 root.geometry('1200x700+200+100')
 root.title('DHT11 Sensor Data Visualisation')
 root.state('zoomed')
 root.config(background='#fafafa')
 
+
+
+
+# Define Plot
 xar = [] # Ordered list of temperature values
 yar = [] # Ordered list of timestamps
-
 style.use('bmh')
 fig = plt.figure(figsize=(14, 4.5), dpi=100)
 ax1 = fig.add_subplot(1, 1, 1)
 ax1.set_ylim(0, 50)
 line, = ax1.plot(xar, yar, 'b', marker='x')
 
+
+# Define Function to add/animate Graph
 def animate(temperature, timestamp):
     yar.append(temperature)
     xar.append(timestamp)
